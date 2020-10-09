654c9579d8a065d93b4db81be9bb00c894f4ae1f
shubwub <shubwub@gmail.com>
2020-10-09 20:23:58 +0100

    changes semantic markup to list over table

--- src/components/molecules/DataTable.tsx
+++ src/components/molecules/DataTable.tsx
@@ -1,22 +1,29 @@
 import React from "react";
+import styles from "./Table.module.scss";
 
 export default function DataTable({ data }: { data: any[] }) {
 	return (
-		<table>
-			<thead>
-				<tr>
-					<th>Metric</th>
-					<th>Value</th>
-				</tr>
-			</thead>
-			<tbody>
-				{data.map((sample, index) => (
-					<tr key={index}>
-						<td style={{ textAlign: "left" }}>{sample.name}</td>
-						<td>{sample.values[sample.values.length - 1]}</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
+		<ul className={styles.table}>
+			<li style={{ display: "flex" }}>
+				<div className={`${styles.tableHead} ${styles.leftTableCell}`}>
+					Metric
+				</div>
+				<div className={`${styles.tableHead} ${styles.rightTableCell}`}>
+					Value
+				</div>
+			</li>
+			{data.map((sample, index) => (
+				<li className={styles.tableRow} key={index}>
+					<div
+						className={`${styles.tableData} ${styles.leftTableCell} ${styles.leftData}`}
+					>
+						{sample.name}
+					</div>
+					<div className={`${styles.tableData} ${styles.rightTableCell}`}>
+						{sample.values[sample.values.length - 1]}
+					</div>
+				</li>
+			))}
+		</ul>
 	);
 }
