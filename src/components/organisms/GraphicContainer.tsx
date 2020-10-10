8034246688388a3e1ad6a616657cc977c305749a
shubwub <shubwub@gmail.com>
2020-10-10 23:32:59 +0100

    moves state higher and adds sorting to table

--- src/components/organisms/GraphicContainer.tsx
+++ src/components/organisms/GraphicContainer.tsx
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { DataSampleInterface } from "../../interfaces";
-import { getData } from "../../services/api";
 import { DataTable, DataGraph } from "../molecules";
 
 import styles from "./GraphicContainer.module.scss";
 
-export default function GraphicContainer() {
-	const [data, setData] = useState<DataSampleInterface[]>([]);
-	const [graphData, setGraphData] = useState<DataSampleInterface[]>([]);
-
-	const fetchData = async () => {
-		const data = await getData();
-		setData(data);
-	};
-
-	useEffect(() => {
-		fetchData();
-	}, []);
-
+export default function GraphicContainer({
+	data,
+	setData,
+	graphData,
+	setGraphData,
+}: {
+	data: any[];
+	setData: any;
+	graphData: any[];
+	setGraphData: any;
+}) {
 	return (
 		<main className={styles.infographic}>
 			{data.length && (
 				<>
-					<DataTable data={data} graphData={graphData} setGraphData={setGraphData}/>
-					<DataGraph data={graphData} />
+					<DataTable
+						data={data}
+						setData={setData}
+						graphData={graphData}
+						setGraphData={setGraphData}
+					/>
+					<DataGraph data={graphData} setGraphData={setGraphData} />
 				</>
 			)}
 		</main>
